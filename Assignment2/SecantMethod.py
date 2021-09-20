de662101206c1f09efee3a88c610bcc6bf71e816
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-09-20 14:32:26 -0400

    7.10 Done

--- Assignment2/SecantMethod.py
+++ Assignment2/SecantMethod.py
@@ -1,56 +1,36 @@
 import math
-# Epsilon = e
+# Epsilon = eps
 # x^-1 = x1
 # x^0 = x0 
 
-# 7.10 A.
+# 7.10 A, B
 
+# let g(x) = (2x-1)^2 + 4(4-1024x)^4
 def function(x):
-    f = (2*x-1)**2 + 4*(4-1024*x)**4
+    f = ( 2*x -1 )**2 + 4 * ( 4 - 1024*x )**4
     return f
 
-
-# Implementing Secant Method
-
-def secant(x0,x1,e,N):
-    print('\n\n*** SECANT METHOD IMPLEMENTATION ***')
+def secant(x0,x1,eps):
+    print('\n Secant Method Implementation for Python')
     while True:
         eps = 0.00001
-        x2 = x0 - (x1-x0)*function(x0)/( function(x1) - function(x0) ) 
+        secantMethod = x0 - ( x1 - x0 ) * function(x0) / ( function(x1 ) - function(x0) ) 
         x0 = x1
-        x1 = x2
-        
+        x1 = secantMethod
+        # condition : abs(x0 - x1) < eps * abs(x0) 
         if abs(x0 - x1) < eps * abs(x0):
-            print('Exit')
+            print("\n abs(x0 - x1) < eps * abs(x0) is false so it's done!")
             break
         
-        condition = abs(function(x2)) > e
-    print('\n Required root is: %0.8f' % x2)
+    print('\n Required root is: %0.8f \n' % secantMethod )
 
-
-# Input Section
+# Initialization
 x0 = 0
 x1 = 1
 eps = 0.00001
-N = input('Maximum Step: ')
 
-# Converting x0 and e to float
-x0 = float(x0)
-x1 = float(x1)
-eps = float(eps)
-
-# Converting N to integer
-N = int(N)
-
-
-#Note: You can combine above three section like this
-# x0 = float(input('Enter First Guess: '))
-# x1 = float(input('Enter Second Guess: '))
-# e = float(input('Tolerable Error: '))
-# N = int(input('Maximum Step: '))
-
-# Starting Secant Method
-secant(x0,x1,eps,N)
+# Start the secant Method
+secant(x0,x1,eps)
 
 
 
