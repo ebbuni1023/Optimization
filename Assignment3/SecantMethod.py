027eb0c43494ffa1797474a7fcf0a8a6880bbe5c
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-10-05 23:55:34 -0400

    update

---
+++ Assignment3/SecantMethod.py
@@ -0,0 +1,37 @@
+import math
+# Epsilon = eps
+# x^-1 = x1
+# x^0 = x0 
+
+# 7.10 A, B
+
+# let g(x) = (2x-1)^2 + 4(4-1024x)^4
+def function(x):
+    # can change the function
+    f = ( 2*x -1 )**2 + 4 * ( 4 - 1024*x )**4
+    return f
+
+def secant(x0,x1,eps):
+    print('\n Secant Method Implementation for Python')
+    while True:
+        eps = 0.00001
+        secantMethod = x0 - ( x1 - x0 ) * function(x0) / ( function(x1 ) - function(x0) ) 
+        x0 = x1
+        x1 = secantMethod
+        # condition : abs(x0 - x1) < eps * abs(x0) 
+        if abs(x0 - x1) < eps * abs(x0):
+            print("\n abs(x0 - x1) < eps * abs(x0) is false so it's done!")
+            break
+        
+    print('\n Root of g(x): %0.8f \n' % secantMethod)
+    print(' Value of g: %0.8f \n' % function(secantMethod))
+# Initialization
+x0 = 0
+x1 = 1
+eps = 0.00001
+
+# Start the secant Method
+secant(x0,x1,eps)
+
+
+
